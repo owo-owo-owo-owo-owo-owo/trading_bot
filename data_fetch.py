bcb13d6b0906081cee9b080bdf9ae8cce92fd95d
owo-owo-owo-owo-owo-owo <matteomazzotti21@gmail.com>
2024-10-20 10:37:57 +0200

    fixed datetime index error

--- data_fetch.py
+++ data_fetch.py
@@ -21,7 +21,9 @@ class DataFetcher:
             # Extract relevant columns and format the data (timestamp, open, high, low, close, volume)
             ohlc_data = [(int(item[0]), float(item[1]), float(item[2]), float(item[3]), float(item[4]), float(item[5])) for item in data]
             print("data retrieved successfully" "\n")
+
             data = pd.DataFrame(ohlc_data,columns=['timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+
             data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
 
             # Define the CSV file name
@@ -30,13 +32,14 @@ class DataFetcher:
             # Check if the file already exists
             if not os.path.exists(csv_filename):
                 # If the file doesn't exist, write the DataFrame to a new file with headers
-                data.to_csv(csv_filename, index=False)
+                data.to_csv(csv_filename, index=True)
                 print(f"File {csv_filename} created and data saved.")
+            '''
             else:
                 # If the file exists, append the data without writing the header again
-                data.to_csv(csv_filename, mode='a', header=False, index=False)
+                data.to_csv(csv_filename, mode='a', header=False, index=True)
                 print(f"Data appended to {csv_filename}.")
-
+            '''
 
             return csv_filename
         else:
